0d5d89d9ab45892ac3e5978e03ced2e5a689114d
AVera2024 <mr.adam.d.vera@gmail.com>
2024-12-11 22:48:53 -0500

    Update booksdb.js

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
-let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
-}
+const books = {
+    1: {"author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": [{"username": "adam", "review": "Great book!"}, {"username": "user2", "review": "Must-read!"}], "isbn": "isbn-1234567890"},
+    2: {"author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": [{"username": "adam", "review": "Magical stories!"}, {"username": "adam1", "review": "Classic tales!"}], "isbn": "isbn-2345678901"},
+    3: {"author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": [{"username": "adam", "review": "Epic masterpiece!"}, {"username": "adam1", "review": "Life-changing!"}], "isbn": "isbn-3456789012"},
+    4: {"author": "Unknown", "title": "The Epic Of Gilgamesh", "reviews": [{"username": "adam", "review": "Ancient treasure!"}, {"username": "adam1", "review": "Fascinating!"}], "isbn": "isbn-4567890123"},
+    5: {"author": "Unknown", "title": "The Book Of Job", "reviews": [{"username": "adam", "review": "Profound wisdom!"}, {"username": "adam1", "review": "Deep insights!"}], "isbn": "isbn-5678901234"},
+    6: {"author": "Unknown", "title": "One Thousand and One Nights", "reviews": [{"username": "adam", "review": "Arabian Nights delight!"}, {"username": "adam1", "review": "Entertaining!"}], "isbn": "isbn-6789012345"},
+    7: {"author": "Unknown", "title": "Nj\u00e1l's Saga", "reviews": [{"username": "adam", "review": "Nordic saga!"}, {"username": "adam1", "review": "Historically rich!"}], "isbn": "isbn-7890123456"},
+    8: {"author": "Jane Austen", "title": "Pride and Prejudice", "reviews": [{"username": "adam", "review": "Classic romance!"}, {"username": "adam1", "review": "Timeless love!"}], "isbn": "isbn-8901234567"},
+    9: {"author": "Honor\u00e9 de Balzac", "title": "Le P\u00e8re Goriot", "reviews": [{"username": "adam", "review": "French masterpiece!"}, {"username": "adam1", "review": "Compelling plot!"}], "isbn": "isbn-9012345678"},
+    10: {"author": "Samuel Beckett", "title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": [{"username": "adam", "review": "Existential journey!"}, {"username": "adam1", "review": "Philosophical depth!"}], "isbn": "isbn-0123456789"}
+};
 
-module.exports=books;
+module.exports = books;
