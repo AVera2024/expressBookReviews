70ee07d038dce61abf5d5e08f31d36ba635a90cb
AVera2024 <mr.adam.d.vera@gmail.com>
2024-12-12 00:53:06 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -10,21 +10,23 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
-if (req.session.authorization) { // Get the authorization object stored in the session
-    token = req.session.authorization['accessToken']; // Retrieve the token from authorization object
-    jwt.verify(token, "access", (err, user) => { // Use JWT to verify token
-      if (!err) {
-        req.user = user;
-        next();
-      } else {
-        return res.status(403).json({ message: "User not authenticated" });
-      }
-    });
-  } else {
-    return res.status(403).json({ message: "User not logged in" });
-  }
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // Middleware which tells that the user is authenticated or not
+    if (req.session.authorization) {
+        let token = req.session.authorization['accessToken']; // Access Token
+
+        jwt.verify(token, "access", (err, user) => {
+            if (!err) {
+                req.user = user;
+                next();
+            }
+            else {
+                return res.status(403).json({ message: "User not authenticated" })
+            }
+        });
+    } else {
+        return res.status(403).json({ message: "User not logged in" })
+    }
 });
 
 // Main endpoint to be accessed by authenticated users
@@ -32,7 +34,7 @@ app.get("/auth/get_message", (req, res) => {
   return res.status(200).json({ message: "Hello, You are an authenticated user. Congratulations!" });
 });
  
-const PORT =5004;
+const PORT =5005;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
