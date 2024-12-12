b145f6f66ea3fd8a54cbe0b1499c0de5a182b826
AVera2024 <mr.adam.d.vera@gmail.com>
2024-12-12 00:56:32 -0500

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
+const session = require('express-session');
 const regd_users = express.Router();
 
 let users = [
@@ -26,30 +27,39 @@ let filtered_users = users.filter((user)=> user.username === user);
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
 let validusers = users.filter((user) => {
-    return user.username === username && user.password === password;
-  });
-  return validusers.length > 0;
-};
+    return (user.username === username && user.password === password)
+});
+if (validusers.length > 0) {
+    return true;
+} else {
+    return false;
+    }
+}
 
 //only registered users can login
-regd_users.post("/login", (req,res) => {
-  //Write your code here
-  const username = req.body.username;
-  const password = req.body.password;
-  if (!username || !password) {
-    return res.status(404).json({ message: "Error logging in" });
-  }
-  if (authenticatedUser(username, password)) {
-    let accessToken = jwt.sign({
-      data: password
-    }, 'access', { expiresIn: 60 * 60 });
-    req.session.authorization = {
-      accessToken, username
-    };
-    return res.status(200).send("User successfully logged in");
-  } else {
-    return res.status(208).json({ message: "Invalid Login. Check username and password" });
-  }
+regd_users.post("/login", (req, res) => {
+    //Write your code here
+    const username = req.query.username;
+    const password = req.query.password;
+
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+
+    if (authenticatedUser(username, password)) {
+        let accessToken = jwt.sign(
+            { data: password },
+            'access',
+            { expiresIn: 60 * 60 }
+        );
+
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
