85049e73eae0d4bff8bd6148dd0bc199bd2bbf30
AVera2024 <mr.adam.d.vera@gmail.com>
2024-12-11 22:48:27 -0500

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -12,9 +12,27 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+if (req.session.authorization) { // Get the authorization object stored in the session
+    token = req.session.authorization['accessToken']; // Retrieve the token from authorization object
+    jwt.verify(token, "access", (err, user) => { // Use JWT to verify token
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
+});
+
+// Main endpoint to be accessed by authenticated users
+app.get("/auth/get_message", (req, res) => {
+  return res.status(200).json({ message: "Hello, You are an authenticated user. Congratulations!" });
 });
  
-const PORT =5000;
+const PORT =5003;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
